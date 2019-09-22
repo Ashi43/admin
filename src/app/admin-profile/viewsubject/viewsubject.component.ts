247e99ce83d78add8cc843dadcad1f7e6b899c9a
sounak1337 <sounakofficial@hotmail.com>
2019-09-22 18:08:39 +0530

    basic componnts added

---
+++ src/app/admin-profile/viewsubject/viewsubject.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-viewsubject',
+  templateUrl: './viewsubject.component.html',
+  styleUrls: ['./viewsubject.component.css']
+})
+export class ViewsubjectComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddsubjectComponent } from './admin-profile/addsubject/addsubject.component';
+import { EditsubjectComponent } from './admin-profile/editsubject/editsubject.component';
+import { AddquestionComponent } from './admin-profile/addquestion/addquestion.component';
+import { EditquestionComponent } from './admin-profile/editquestion/editquestion.component';
+import { ViewquestionComponent } from './admin-profile/viewquestion/viewquestion.component';
+import { ViewsubjectComponent } from './admin-profile/viewsubject/viewsubject.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddsubjectComponent,
+    EditsubjectComponent,
+    AddquestionComponent,
+    EditquestionComponent,
+    ViewquestionComponent,
+    ViewsubjectComponent
   ],
   imports: [
     BrowserModule,
